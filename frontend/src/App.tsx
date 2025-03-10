fc7e7510041f195bc82eb0219e3d363d5e797956
ANAS727189 <anas23khan083@gmail.com>
2025-03-10 10:24:41 +0530

    feat: changed from svelte to react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="container mx-auto">
+	<div className="flex flex-col justify-center items-center h-screen space-y-6 text-center">
+		<img src="/favicon.png" alt="logo" className="w-24 h-24" />
+		<h1 className="text-5xl font-bold">Hungrr</h1>
+		<div className="space-y-4">
+			<h1 className="text-4xl font-semibold">🚧 Something is Cooking 🚧</h1>
+			<h3 className="text-gray-500 text-lg">Come back tomorrow to see the website...</h3>
+		</div>
+	</div>
+</div>
+    </>
+  )
+}
+
+export default App
